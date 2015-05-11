666c399b25f9babf815cc8e7fbc1bec56cdeb265
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-05-10 21:38:20 -0500

    initial variables & types in place

--- bp_track.py
+++ bp_track.py
@@ -1,2 +1,12 @@
+import csv
+
 systolic = int(input("Enter your systolic pressure: "))
 dias = int(input("Enter your diastolic pressure: "))
+heart = int(input("Enter your heart rate: "))
+
+#Capturing the date/time information as strings for now. I'll work out
+#converting them into date/time objects later. Probably after I 
+#connect this to a database. Now it's just text logging
+bp_date = str(input("Enter the date in ISO format (2015-05-09)--> "))
+bp_time = str(input("Enter the time in ISO format (18:26:26)--> "))
+
