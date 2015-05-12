a00c0c95342f90ff543e051f94a1e9d9be1d169d
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-05-11 22:47:57 -0500

    added a few new variables to track.

--- bp_track.py
+++ bp_track.py
@@ -10,3 +10,11 @@ heart = int(input("Enter your heart rate: "))
 bp_date = str(input("Enter the date in ISO format (2015-05-09)--> "))
 bp_time = str(input("Enter the time in ISO format (18:26:26)--> "))
 
+#Realized I should capture weight and number of steps for the day
+#Weight will be in Kilos; I can convert to LBs later if I want
+##at some point, I'll want to add something that asks 
+##which unit you're using
+weight_kg = int(input("Enter your weight in kilograms: "))
+#A proxy for exercise/activity; a trend worth tracking to correlate w/
+#blood pressure fluctuations
+steps = int(input("Enter your steps for the day: "))
