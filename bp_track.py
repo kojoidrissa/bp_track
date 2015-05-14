5209d2b5a5bbfecac3f180fda777a6bed7a8d04a
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-05-13 19:35:16 -0500

    moved dates to the top of the data being captured

--- bp_track.py
+++ bp_track.py
@@ -1,14 +1,15 @@
 import csv
 
-systolic = int(input("Enter your systolic pressure: "))
-dias = int(input("Enter your diastolic pressure: "))
-heart = int(input("Enter your heart rate: "))
-
 #Capturing the date/time information as strings for now. I'll work out
 #converting them into date/time objects later. Probably after I 
 #connect this to a database. Now it's just text logging
 bp_date = str(input("Enter the date in ISO format (2015-05-09)--> "))
 bp_time = str(input("Enter the time in ISO format (18:26:26)--> "))
+
+systolic = int(input("Enter your systolic pressure: "))
+dias = int(input("Enter your diastolic pressure: "))
+heart = int(input("Enter your heart rate: "))
+
 
 #Realized I should capture weight and number of steps for the day
 #Weight will be in Kilos; I can convert to LBs later if I want
